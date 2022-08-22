140d04bf62bb9f4b2eaad02a7230c95beed63fe8
Deepaktenzi <Deepaktenzi@gmail.com>
2022-08-22 13:01:08 +0530

    Code Added

---
+++ Practice/215. Kth Largest Element in an Array/solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class solution {
+
+    static int findKthLargest(int[] nums, int k) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+            for(int i = 0; i < k; i++){
+                pq.add(nums[i]);     
+            }
+            for(int i = k; i<nums.length;i++){
+                if(pq.peek()< nums[i]){
+                    pq.poll();
+                    pq.add(nums[i]);
+
+                }
+            }
+        return pq.peek();
+    }
+    public static void main(String[] args) {
+        int num[]={3,2,1,5,6,4};
+        int k = 2;
+        findKthLargest(num,k);
+    }
+}
