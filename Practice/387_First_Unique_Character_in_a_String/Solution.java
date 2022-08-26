5f0fd7f48c6030025143afc472f92c7ebb1672b0
Deepaktenzi <Deepaktenzi@gmail.com>
2022-08-26 08:16:47 +0530

    Code Added

--- Practice/387_First_Unique_Character_in_a_String/Solution.java
+++ Practice/387_First_Unique_Character_in_a_String/Solution.java
@@ -1,3 +1,12 @@
+/*Given a string s, find the first non-repeating character in it and return its index. 
+If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+*/
+ 
 import java.util.HashMap;
 
 public class Solution {
