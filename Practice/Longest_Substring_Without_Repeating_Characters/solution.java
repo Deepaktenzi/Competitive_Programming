06a25d898fc99225ecc634f648917979491a7a49
Deepak Negi <deepaktenzi@gmail.com>
2022-10-19 16:23:29 +0530

    New Solution

--- Practice/Longest_Substring_Without_Repeating_Characters/solution.java
+++ Practice/Longest_Substring_Without_Repeating_Characters/solution.java
@@ -1,3 +1,11 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
 package Longest_Substring_Without_Repeating_Characters;
 import java.util.*;
 public class solution{
